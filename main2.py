fca90c137e98318209a02c23bb9c05c8e079398a
sidney2k1 <sidsingla02@gmail.com>
2024-12-24 20:27:47 +0530

    main2

---
+++ main2.py
@@ -0,0 +1,9 @@
+def Ontime(n):
+    iterations=0
+    for i in range(1,n+1):
+        iterations+=1
+    print("when n is",n,"iterations=",iterations)
+Ontime(10)
+Ontime(20)
+Ontime(30)
+print("With increase in n the number of iterations will also increase")
