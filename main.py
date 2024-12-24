e6dc6969909518cf1dc719f8b4a36bbc1cf39cfc
sidney2k1 <sidsingla02@gmail.com>
2024-12-24 20:22:44 +0530

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iterations=0
+    print("the number entered by the user is:",n)
+    iterations+=1
+    print("Total iterations done by the code is:",iterations)
+printnumber(10)
+printnumber(20)
+print("With any number the iterations will remain the same")
